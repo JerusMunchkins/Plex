329feebb57271ce339e50de1623a413ac9ea90b5
jeru <mikurej95@gmail.com>
2018-10-24 15:58:59 -0500

    removing a home no longer crashes app and properly updates ranking tabs

--- client/components/rankingTabs/RankingTabs.js
+++ client/components/rankingTabs/RankingTabs.js
@@ -105,6 +105,9 @@ class RankingTabs extends React.Component {
 
               const color = i % 2 === 0 ? 'light-blue' : 'blue'
               const selected = value === i ? 'selected' : ''
+
+              if (!home[0]) return
+
               return (
                 <li
                   key={home[0].id}
